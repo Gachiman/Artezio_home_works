9d627cc25c8453ac0a95775c4ba39aa9a45d5de9
Ilya Aglikov <aglikov@bk.ru>
2019-07-10 22:06:20 +0300

    Tasks 1 done, now i can take a rest

---
+++ JS/Task 1/script.js
@@ -0,0 +1,17 @@
+for (var i = 10; i <= 20; i++) {
+    console.log(i);
+}
+
+// First task
+console.log("\nSquares of a numbers:");
+for (var i = 10; i <= 20; i++) {
+    console.log(i * i);
+}
+
+// Second task
+var sum = 0;
+for (var i = 10; i <= 20; i++) {
+    sum += i;
+}
+sum = "\nThe sum of the numbers from 10 to 20 is: " + sum;
+console.log(sum);
