d345a8327ad489445d9ce5499db4660c2194d2d9
Ilya Aglikov <aglikov@bk.ru>
2019-07-12 14:35:43 +0300

    Memory game V1.0.1

--- JS/Task 2/game_script.js
+++ JS/Task 2/game_script.js
@@ -1,9 +1,11 @@
-var picArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+// JS вижу впервые, пожалуйста не бейте за плохой код
+
+var picArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];  // Массив индексов изображений
 mixArray(picArray);
 
-var winArray = [];
+var winArray = [];  // Будет содержать индексы плиток, которые уже отгадали
 
-function mixArray(array) {
+function mixArray(array) {  // Перемешать массив карт
     var j;
     for (var i = 0; i < array.length; i++) {
         j = Math.floor(Math.random() * (array.length - 1));
@@ -11,29 +13,27 @@ function mixArray(array) {
     }
 }
 
-var clickTick = 0;
-var prevNum = -1;
+var clickTick = 0;  // Первая карта открыта - равно 1, вторая открыта - уменьшается до нуля
+var prevNum = -1;  // Индекс первой открытой карты
+var flag = false;  // Замок, не позволяющий открывать карты, пока не закроются открытые
 
-function squareClick(number) {
-    if (number == prevNum) {
-        //closePic(number);
+function squareClick(number) {  // Обрабатывает нажатие
+    // Если открываем уже открытую карту, либо ещё не закрылись открытые, либо карта уже отгадана
+    if (number == prevNum || flag == true || winArray.indexOf(number) != -1) {
         return;
     }
-    if (winArray.indexOf(number) != -1) {
-        return;
-    }
-    if (clickTick == 0) {
+    if (clickTick == 0) {  // Открываем первую карту
         clickTick++;
         openPic(number);
         prevNum = number;
-    } else {
+    } else {  // Открываем вторую карту
+        flag = true;
         clickTick--;
         openPic(number);
         
-        if (picArray[number] != picArray[prevNum]) {
+        if (picArray[number] != picArray[prevNum]) {  // Не отгадали
             setTimeout(closePictures, 1000, number, prevNum);
-        } else {
-            alert('Yes');
+        } else {  // Отгадали
             winArray.push(number);
             winArray.push(prevNum);
         }
@@ -44,6 +44,7 @@ function squareClick(number) {
 function closePictures(number, prevNum) {
     closePic(number);
     closePic(prevNum);
+    flag = false;
 }
 
 function openPic(number) {
